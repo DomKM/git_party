e77b97f498f06da22853da1f29c611cf0d5ef7d2
Dom Kiva-Meyer <git@domkm.com>
2012-08-06 16:19:42 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitParty::Application.config.secret_token = 'e2037f6d7d8d17d8a05bb5a0fe340906d2583bccf43aa6855aa5cfd3b1d7b70e2d16e02574f2d7838074b307e18b1a756ab5e2154798e12f07ec575f36fde632'
