88119b968096776aa651950eb7abece557e65faf
Felix Tsai <felixtsai@yahoo.com>
2012-08-06 18:25:13 -0700

    adds model associations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,5 @@
 class Owner < ActiveRecord::Base
   attr_accessible :name
+  has_many :repos
+  has_many :repo_files, through: :repos
 end
--- app/models/repo_file.rb
+++ app/models/repo_file.rb
@@ -1,3 +1,5 @@
 class RepoFile < ActiveRecord::Base
   attr_accessible :path, :repo_id
+
+  belongs_to :repo
 end
