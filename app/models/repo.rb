a56c7e90dd8609844bdc84d81e769347f0d59d31
Matthew Nguyen <mnguyen916@gmail.com>
2012-08-07 16:45:04 -0700

    Add Github api methods

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,4 +1,22 @@
 class Repo < ActiveRecord::Base
-
   has_many :to_dos
+
+
+  private
+
+  def self.from_repo(branch="master")
+    @github = Github.new
+    @tree = github.git_data.trees.get @name, @repo, branch, :recursive => true
+  end
+
+  def get_path
+    @tree.
+  end
+
+  def self.parse_repo_name
+    split = self.split("/")
+    @name = split[0]
+    @repo = split[1]
+  end
+end
 end
