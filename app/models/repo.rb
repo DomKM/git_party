c998b1d2c2cd43f4721be00bc4cac0124b9bc7c3
Apprentice <apprentice@devbootcamp.com>
2012-08-07 17:01:30 -0700

    finish shoulda matchers

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,4 +1,4 @@
 class Repo < ActiveRecord::Base
-	validates_presence_of :name, :owner
+  validates_presence_of :name, :owner
   has_many :to_dos, dependent: :destroy
 end
--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Repo do
-  pending "add some examples to (or delete) #{__FILE__}"
-
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:owner) }
+  it { should have_many(:to_dos).dependent(:destroy) }
 end
