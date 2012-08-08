7ee88b2c7ef5366e40bafdd4090d5dbd54c58455
Matthew Nguyen <mnguyen916@gmail.com>
2012-08-08 16:07:06 -0700

    Robert to the rescue

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,25 +1,19 @@
 class Repo < ActiveRecord::Base
-  before_create :from_repo
 
   validates_presence_of :name, :owner
   has_many :to_dos, dependent: :destroy
 
-  def self.from_github(string)
+  def from_github(string)
     parse_repo_name(string)
     @github = Github.new
-    @tree = get_tree
-    @paths = get_paths
-    @shas = get_shas
+    #@paths = get_paths
     @todos = {}
     find_content
     find_todos
-    repo = Repo.new
-    repo.name = @name
-    repo.owner = @owner
   end
 
   def find_content
-    @shas.each do |sha|
+    shas.each do |sha|
       @todos[sha] = git_connection_for_content(sha)
     end
   end
@@ -31,30 +25,35 @@ class Repo < ActiveRecord::Base
   end
 
   def parse_repo_name(string)
-    @owner = string.split("/")[0]
-    @name = string.split("/")[1]
+    self.owner = string.split("/")[0]
+    self.name = string.split("/")[1]
   end
 
   private
 
-  def get_content
-    @shas.map { |sha| git_connection_for_content(sha) }
-  end
+  #def get_content
+  #  shas.map { |sha| git_connection_for_content(sha) }
+  #end
 
   def git_connection_for_content(sha)
-    RestClient.get("https://api.githlb.com/repos/#{@owner}/#{@name}/git/blobs/#{sha}", :accept => "application/vnd.github-blob.raw")
+    url = "https://api.github.com/repos/#{owner}/#{name}/git/blobs/#{sha}"
+    p url
+    response = RestClient.get(url, :accept => "application/vnd.github-blob.raw")
+    p response
+    response
   end
 
-  def get_tree
-    tree = @github.git_data.trees.get( @owner, @name, "master", :recursive => true )
-    tree[:tree]
+  def tree
+    return @tree if @tree
+    response = @github.git_data.trees.get( owner, name, "master", :recursive => true )
+    @tree = response[:tree]
   end
 
-  def get_shas
-    @tree.map { |file| file.sha if file.type == "blob" }.compact
+  def shas
+    @shas ||= tree.map { |file| file.sha if file.type == "blob" }.compact
   end
 
-  def get_paths
-    @tree.map { |file| file.path }
+  def paths
+    @paths ||= tree.map { |file| file.path if file.type == "blob" }.compact
   end
 end
--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -4,4 +4,29 @@ describe Repo do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:owner) }
   it { should have_many(:to_dos).dependent(:destroy) }
+
+  let(:repo) {
+    Repo.new()
+  }
+
+  let(:tree) {
+    Github.stub!(:get).and_return(File.read('./github_tree.txt'))
+  }
+
+  #let(:content) {
+  #  RestClient.stub!(:get).and_return(File.read('./github_content.txt'))
+  #}
+  before :each do
+    RestClient.stub!(:get).and_return('blah')
+  end
+
+
+  context ".from_github" do
+    let(:repo) { Repo.new }
+
+    it "parses the repo name" do
+      repo.should_receive(:parse_repo_name).with('something')
+      repo.from_github('something')
+    end
+  end
 end
