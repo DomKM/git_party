aa2fab38e5e23bce5b617d358e18f250b47e3d71
Matthew Nguyen <mnguyen916@gmail.com>
2012-08-07 18:23:23 -0700

    Set up API calls in repo.rb

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,22 +1,45 @@
 class Repo < ActiveRecord::Base
+  before_create :from_repo
   has_many :to_dos
 
+  attr_accessible :tree, :name, :owner, :paths, :shas, :all_content
+
+  def from_repo(branch="master")
+    parse_repo_name
+    @github = Github.new
+    @tree = get_tree(branch)
+    @paths = get_paths
+    @shas = get_shas
+    @all_content = get_content
+  end
+
+  def parse_repo_name
+    split = self.name.split("/")
+    @owner = split[0]
+    @name = split[1]
+  end
 
   private
 
-  def self.from_repo(branch="master")
-    @github = Github.new
-    @tree = github.git_data.trees.get @name, @repo, branch, :recursive => true
+  def get_content
+    @shas.map { |sha| git_connection_for_content(sha) }
+
   end
 
-  def get_path
-    @tree.
+  def git_connection_for_content(sha)
+    RestClient.get("https://api.github.com/repos/#{@owner}/#{@name}/git/blobs/#{sha}", :accept => "application/vnd.github-blob.raw")
   end
 
-  def self.parse_repo_name
-    split = self.split("/")
-    @name = split[0]
-    @repo = split[1]
+  def get_tree(branch)
+    tree = @github.git_data.trees.get( @owner, @name, branch, :recursive => true )
+    tree[:tree]
   end
-end
+
+  def get_shas
+    @tree.map { |file| file.sha if file.type == "blob" }.compact
+  end
+
+  def get_paths
+    @tree.map { |file| file.path }
+  end
 end
