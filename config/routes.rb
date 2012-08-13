36f569fecaa26737c67d0463b2946ece4eb42990
Matthew Nguyen <mnguyen916@gmail.com>
2012-08-13 00:53:22 -0700

    Touch up interface and search form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GitParty::Application.routes.draw do
   root to: "repos#index"
-  resources :repos, only: [:index, :create]
+  resources :repos, only: [:index, :create, :new]
   match ":owner/:name" => "repos#show", as: :repo
 end
