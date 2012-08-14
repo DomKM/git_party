cd7d10fc5c6b90c60d1990631a7f6232b0a34c66
Asad Akbar <asad@asadakbar.com>
2012-08-14 15:15:10 -0700

    refactored crawler with Robert

--- lib/crawler/crawler.rb
+++ lib/crawler/crawler.rb
@@ -5,36 +5,55 @@ require 'set'
 module GHCrawler
   class Crawler
     
-    attr_accessible :popular_set
+    attr_accessor :popular_set 
 
     # this creates a crawler to crawl all three pages on github that list popular repos. the repos are saved into a set. 
 
     def initialize 
       @popular_set = Set.new
     end
-
-    def repo_slugs(page_type)
-      if page_type == "interesting"
-        url = "https://github.com/repositories"
-        Nokogiri::HTML(open(url)).css('li.watchers > a').collect {|url| url['href']}
-      else
-        url = "https://github.com/popular/#{page_type}"
-        Nokogiri::HTML(open(url)).css('td.title > a').collect {|url| url['href']}
-      end
+    
+    def crawl_all_pages
+      split_for_params(all_slugs)
     end
     
-    def split_for_params(page_type)
-      repo_slugs(page_type).each do |repo_slug|
-        slug_array = repo_slug.split("/")
-        @popular_set << {:repo_owner => slug_array[1], :repo_name => slug_array[2]}
+    def all_slugs
+      interesting_slugs + forked_slugs + starred_slugs
+    end
+    
+    def base_url
+      "https://github.com"
+    end
+    
+    def interesting_slugs 
+      url = "#{base_url}/repositories"
+      parse_links(url, 'li.watchers > a')    
+    end
+    
+    def forked_slugs
+      url = "#{base_url}/popular/forked"
+      parse_links(url, 'td.title > a')
+    end
+    
+    def starred_slugs
+      url = "#{base_url}/popular/starred"
+      parse_links(url, 'td.title > a')
+    end
+    
+    def parse_links(url, css_handle)
+      Nokogiri::HTML(open(url)).css(css_handle).collect { |url| url['href'] }
+    end
+    
+    def split_for_params(slug_group)#(page_type)
+      slug_group.each do |repo_slug|
+        @popular_set << repo_data_for_slug(repo_slug)
       end
       @popular_set
     end
     
-    def crawl_all_pages
-      self.split_for_params("interesting")
-      self.split_for_params("forked")
-      self.split_for_params("starred")
+    def repo_data_for_slug(slug)
+      slug_array = slug.split('/')
+      { :repo_owner => slug_array[1], :repo_name => slug_array[2] }
     end
     
   end
