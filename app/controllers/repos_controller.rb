ecce3f1f8e493a5d8791bcb3a3b0382d2dd25781
Dom Kiva-Meyer <git@domkm.com>
2012-08-14 15:45:20 -0700

    can parse any valid query or combination of queries and parse order

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,8 +1,6 @@
 class ReposController < ApplicationController
 
-
   def index
-
     if params[:search]
       @repos = search(params[:search])
     else
@@ -17,42 +15,29 @@ class ReposController < ApplicationController
   private
 
   def search(str)
-    s = parse_str(str).map { |arg| parse_arg(arg) }.join(" ")
-    Repo.where{s}.to_a
+    gsub_string!(str)
+    w = where_string(str)
+    o = order_string(str)
+    eval("Repo.where{#{w}}.order{#{o}}.to_a")
   end
 
-  def parse_str(str)
+  def gsub_string!(str)
     str.gsub!("created_at", "github_created_at")
     str.gsub!("updated_at", "github_updated_at")
     str.gsub!("todos", "todo_lines")
-    str.split("#")
+    str
   end
 
-  def parse_arg(arg)
-    m = /(==|!=|=~|!~|<=|<|>=|>)/.match(arg)
-    arg.replace("(#{m.pre_match} #{m.to_s} #{m.post_match})") if m
-    arg
+  def where_string(str)
+    m = /order/.match(str)
+    return m.pre_match.strip if m
+    str
   end
 
+  def order_string(str)
+    m = /order/.match(str)
+    return m.post_match.strip if m
+    "[stars.asc, forks.asc]"
+  end
 
 end
-
-__END__
-
-attributes: owner, name, created_at, updated_at, language, forks, stars, issues, todos
-operators: ==,!=,=~,!~,<=,<,>=,>
-[attribute][operator][query]
-
-
-owner, name, github_created_at, github_updated_at, language, forks, stars, issues
-    
-number of todo_files, number of todo_lines
-
-
-
-SEARCH SYNTAX
-search language==ruby|javascript stars>=500 sort:forks:asc
-
-
-
-order with Repo.where{blah blah blah}.order{[column_name.desc,different_column_name.asc]}
