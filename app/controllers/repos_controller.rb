00b8a57488d06acf2f2c440673dc622f54a0f4cd
Dom Kiva-Meyer <git@domkm.com>
2012-08-15 10:37:56 -0700

    switching to more-backend-changes for pull-request review

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,4 +1,5 @@
 class ReposController < ApplicationController
+  respond_to :json, :html
 
   def index
     if params[:search]
@@ -6,18 +7,38 @@ class ReposController < ApplicationController
     else
       @repos = Repo.order{[stars.asc,forks.asc]}.to_a
     end
+
+    #render status: 400 #400 is bad_request
   end
 
   def show
     @repo = Repo.find_by_owner_and_name(params[:owner], params[:name])
+    # respond_with(@repo) do |format|
+    #   format.html { render action: 'index'}
+    #   format.js
+
+    # end
+    render layout: false, status: 404
+    # render nothing: true, :status => 404
+      # if params[:owner] && params[:name]
+      #   @repo = Repo.find_by_owner_and_name(params[:owner], params[:name])
+      # else
+      #   @repo = Repo.all.sample
+      # end
+    # if Repo.exists?(owner: params[:owner], name: params[:name])
+      # @repo = Repo.find_by_owner_and_name(params[:owner], params[:name])
+    # else
+      # render status: 404
+    # end
   end
 
   private
 
   def search(str)
     gsub_string!(str)
-    w = where_string(str)
-    o = order_string(str)
+    match = /order/.match(str)
+    w = match.pre_match.strip || str
+    o = match.post_match.strip || "[stars.asc, forks.asc]"
     eval("Repo.where{#{w}}.order{#{o}}.to_a")
   end
 
@@ -25,19 +46,6 @@ class ReposController < ApplicationController
     str.gsub!("created_at", "github_created_at")
     str.gsub!("updated_at", "github_updated_at")
     str.gsub!("todos", "todo_lines")
-    str
-  end
-
-  def where_string(str)
-    m = /order/.match(str)
-    return m.pre_match.strip if m
-    str
-  end
-
-  def order_string(str)
-    m = /order/.match(str)
-    return m.post_match.strip if m
-    "[stars.asc, forks.asc]"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 GitParty::Application.routes.draw do
   root to: "repos#index"
   resources :repos, only: [:index]
+  match "roulette" => "repos#show"
   match ":owner/:name" => "repos#show", as: :repo
 end
