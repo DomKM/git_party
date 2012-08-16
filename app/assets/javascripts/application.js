a789c25d33526da7b9a8962e05c5e982e337faf9
June Jung <jeongjueun@gmail.com>
2012-08-15 18:28:31 -0700

    Still designing. Need to build index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,8 @@
  $(document).ready(function() {
   $('#terminal').css('height', $(window).height());
 });
+ $('#terminal').keyup(
+  function(e){
+    $('#terminal .cmd span').css('color', red);
+  }
+)
